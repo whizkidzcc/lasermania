885cbb8c34e6fc676d112231001a68764edbd1d7
Kaotic DonutChicken <kaoticdonutchicken@Macbook-pro.hsd1.ca.comcast.net>
2017-11-15 19:11:08 -0800

    pulling it

---
+++ hello.py
@@ -0,0 +1,6 @@
+#!/usr/local/bin/python3
+
+print('Content-type: text/plain');
+print()
+
+print('Hello world!');
---
+++ rnd.py
@@ -0,0 +1,23 @@
+#!/usr/local/bin/python3
+
+import random
+
+num = random.randint(1, 100)
+
+print('Content-type: text/html');
+print('')
+
+print('''\
+	<!DOCTYPE html>
+	<html>
+	    <head>
+	       <meta charset="UTF8">
+	       <title>Random Number Generator</title>
+	    </head>
+
+	    <body>
+	       <h1>Random Number Generator</h1>
+	       <p> ''' + str(num) + '''</p>
+	    </body>
+</html>
+''')
