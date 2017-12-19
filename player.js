cc9a34cb871576a2a2493cfee2ec156ffb6d8e77
Kaotic DonutChicken <kaoticdonutchicken@macbook-pro.attlocal.net>
2017-12-18 19:54:43 -0800

    modified the files a bit

--- player.js
+++ player.js
@@ -3,8 +3,11 @@ document.addEventListener('DOMContentLoaded', function() {
 	
 	Game.init();
 	
-	var player = new Sprite(['alien.png'], 'playing-area', 'player');
+	var player = new Sprite(['person.png'], 'playing-area', 'player');
 	Game.addSprite(player);
+        
+        var player2 = new Sprite(['person.bmp'], 'playing-area', 'player2');
+	Game.addSprite(player2);
 	
 	player.addKeyBinding(Game.KEY_D, function() {player.changeX(5)});
 	player.addKeyBinding(Game.KEY_A, function() {player.changeX(-5)});
@@ -12,4 +15,13 @@ document.addEventListener('DOMContentLoaded', function() {
 	player.addKeyBinding(Game.KEY_S, function() {player.changeY(5)});
 	player.setBoundary(0, 768, 0, 1366);
 	Game.start();
+        
+        player2.addKeyBinding(Game.KEY_L, function() {player2.changeX(5)});
+	player2.addKeyBinding(Game.KEY_J, function() {player2.changeX(-5)});
+	player2.addKeyBinding(Game.KEY_I, function() {player2.changeY(-5)});
+	player2.addKeyBinding(Game.KEY_K, function() {player2.changeY(5)});
+	player2.setBoundary(0, 768, 0, 1366);
+	Game.start();
+        
+        
 });
