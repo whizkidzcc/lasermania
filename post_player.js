c1a3a60be966868efc6dedc36fa59fd1af1acf94
Immortal747 <raggie0520141@gmail.com>
2018-02-28 18:55:01 -0800

    kfjsl idkjblwiuekj.snfbljwasdc

--- post_player.js
+++ post_player.js
@@ -1,31 +1,25 @@
 var ws;
+var players = {};
 
 function initWebSocket() {
 	ws = new WebSocket("ws://127.0.0.1:5678");
-	ws.onmessage = function(event) {
-		console.log(event.data);
+ws.onmessage = function(event) {
+		var data = JSON.parse(event.data);
+		var name = Object.keys(data)[0];
+		var p;
+		if (!(name in players)) {
+			p = new Sprite(['person.png'], 'playing area', 'player');
+			Game.addSprite(p);
+			players[name] = p;
+		}
+		
+		p = players[name];
+		p.setPosition(data[name]['x'], data[name]['y']);
 	};
 }
 
-//Noo noo depercaterd!!!!!!1111
-/*
-function postPlayer(name, x, y) {
-	var xhttp = new XMLHttpRequest();
-	xhttp.onreadystatechange = function() {
-	if (this.readyState == XMLHttpRequest.DONE && this.status === 200) {
-		document.querySelector('#message').innerHTML = this.responseText;
-	}
-	};
-
-	xhttp.open('POST', 'post.py');
-	xhttp.setRequestHeader('Content-Type', 'text/plain');
-	
-	data = {};
-	data[name] = {"x": x, "y": y};
-	xhttp.send(JSON.stringify(data));
-	//console.log(JSON.stringify(data));
 }
-*/
+
 function postData(name, x, y) {
 	var data = {};
 	data[name] = {'x': x, 'y': y};
